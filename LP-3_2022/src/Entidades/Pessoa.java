454c1712f2fcecd250f471136a41ad38157c74c4
JoaoAN2 <joao.an.221203@gmail.com>
2022-04-24 13:04:56 -0300

    LP-3_2022, início da conexão com o banco de dados

---
+++ LP-3_2022/src/Entidades/Pessoa.java
@@ -0,0 +1,100 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Entidades;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author joaoan2
+ */
+@Entity
+@Table(name = "pessoa")
+@NamedQueries({
+    @NamedQuery(name = "Pessoa.findAll", query = "SELECT p FROM Pessoa p")})
+public class Pessoa implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @Column(name = "id_pessoa")
+    private Integer idPessoa;
+    @Basic(optional = false)
+    @Column(name = "nome_pessoa")
+    private String nomePessoa;
+    @Basic(optional = false)
+    @Column(name = "altura_pessoa")
+    private double alturaPessoa;
+
+    public Pessoa() {
+    }
+
+    public Pessoa(Integer idPessoa) {
+        this.idPessoa = idPessoa;
+    }
+
+    public Pessoa(Integer idPessoa, String nomePessoa, double alturaPessoa) {
+        this.idPessoa = idPessoa;
+        this.nomePessoa = nomePessoa;
+        this.alturaPessoa = alturaPessoa;
+    }
+
+    public Integer getIdPessoa() {
+        return idPessoa;
+    }
+
+    public void setIdPessoa(Integer idPessoa) {
+        this.idPessoa = idPessoa;
+    }
+
+    public String getNomePessoa() {
+        return nomePessoa;
+    }
+
+    public void setNomePessoa(String nomePessoa) {
+        this.nomePessoa = nomePessoa;
+    }
+
+    public double getAlturaPessoa() {
+        return alturaPessoa;
+    }
+
+    public void setAlturaPessoa(double alturaPessoa) {
+        this.alturaPessoa = alturaPessoa;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idPessoa != null ? idPessoa.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Pessoa)) {
+            return false;
+        }
+        Pessoa other = (Pessoa) object;
+        if ((this.idPessoa == null && other.idPessoa != null) || (this.idPessoa != null && !this.idPessoa.equals(other.idPessoa))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return idPessoa + ";" + nomePessoa + ";" + alturaPessoa;
+    }
+    
+}
---
+++ LP-3_2022/src/Main/Main.java
@@ -0,0 +1,17 @@
+package Main;
+
+import GUIs.GUIPessoa;
+
+/**
+ *
+ * @author joaoan2
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        GUIPessoa guiPessoa = new GUIPessoa();
+        
+    }
+    
+}
