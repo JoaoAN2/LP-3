0babcf564e20ce5811fb8a365243e13508c4032e
JoaoAN2 <joao.an.221203@gmail.com>
2022-05-15 20:12:17 -0300

    Ajustes na aplicação CRUD Chess

--- LP3_Chess/src/Entidades/City.java
+++ LP3_Chess/src/Entidades/City.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Entidades;
 
 import java.io.Serializable;
@@ -20,7 +16,7 @@ import javax.persistence.Table;
 
 /**
  *
- * @author joaoan2
+ * @author JoaoAN2
  */
 @Entity
 @Table(name = "city")
---
+++ LP3_Chess/src/Tools/LerJava.java
@@ -0,0 +1,62 @@
+package Tools;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author joaoan2
+ */
+public class LerJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        /**
+         * List<String> atributos = new ArrayList(); // Lista de atributos.
+         * String className = "Cargo"; // Nome da classe.
+         *
+         * // Atributos da classe. atributos.add("int;idFunction;11");
+         * atributos.add("String;nome_functionl;45");
+         *
+         * @Column(name = "sigla_federation") 
+         * private String siglaFederation;
+         *
+         *
+         */
+        String path = "src/Entidades/Federation.java";
+        String nameFile = path.split("/")[path.split("/").length - 1];
+        List<String> atributos = new ArrayList();
+
+        ManipulaArquivo manipulaArquivo = new ManipulaArquivo();
+        List<String> list = manipulaArquivo.abrirArquivo(path);
+
+        for (int i = 0; i < list.size(); i++) {
+
+            if (list.get(i).trim().length() >= 7 && list.get(i).trim().substring(0, 7).equals("@Column")) {
+                String nextLine = list.get(i + 1).trim();
+                String atributo = nextLine.substring(8, nextLine.length() - 1).replaceAll(" ", ";");
+                
+                String aux[] = atributo.split(";");
+                
+                switch (aux[0]){
+                    case "String":
+                        atributo += ";255";
+                        break;
+                    default:
+                        atributo += ";11";
+                }
+                
+                atributos.add(atributo);
+            }
+        }
+
+        for (String lineAtributes : atributos) {
+            System.out.println(lineAtributes);
+        }
+
+    }
+
+}
