eb18e2d9e0ac0640aebef31f3b72053fa0da6e2e
JoaoAN2 <joao.an.221203@gmail.com>
2022-05-15 20:14:54 -0300

    Teste do gerador de CRUD com projeto base LP-3 (Trabalhador)

--- LP-3_2022/src/Main/Main.java
+++ LP-3_2022/src/Main/Main.java
@@ -1,16 +1,80 @@
 package Main;
-
-import GUIs.GUIPessoa;
+import GUIs.TrabalhadorGUI;
+import Tools.ManipulaArquivo;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
- * @author joaoan2
+ * @author JoaoAN2
  */
 public class Main {
-    
+
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
+        String path = "src/Entidades/Trabalhador.java";
+        String nameFile = path.split("/")[path.split("/").length - 1];
+        List<String> atributos = new ArrayList();
+
+        ManipulaArquivo manipulaArquivo = new ManipulaArquivo();
+        List<String> entitieFile = manipulaArquivo.abrirArquivo(path);
+        String className = nameFile.substring(0, nameFile.length() - 5);        
+        List<String> cgEntitie = new ArrayList(); // Entidade
         
-        GUIPessoa guiPessoa = new GUIPessoa();
+        int cont = 0;
+
+        for (int i = 0; i < entitieFile.size(); i++) {
+
+            if (i > 1 && entitieFile.get(i - 1).trim().length() >= 26 && entitieFile.get(i - 1).trim().substring(0,26).equals("public String toString() {")) {
+                
+                String toString = "        return ";
+                
+                for (int j = 0; j < atributos.size(); j++) {
+                    toString += atributos.get(j).split(";")[1] + " + \";\" + ";
+                }
+                
+                toString = toString.substring(0, toString.length() - 9) + ";";
+                
+                cgEntitie.add(toString);
+            } else {
+                cgEntitie.add(entitieFile.get(i));
+            }
+            
+            if (entitieFile.get(i).trim().length() >= 7 && entitieFile.get(i).trim().substring(0, 7).equals("@Column") && cont != 0) {
+                String nextLine = entitieFile.get(i + 1).trim();
+                String atributo = nextLine.substring(8, nextLine.length() - 1).replaceAll(" ", ";");
+                
+                String aux[] = atributo.split(";");
+                
+                switch (aux[0]){
+                    case "String":
+                        atributo += ";255;";
+                        break;
+                    default:
+                        atributo += ";11;";
+                }
+                
+                atributos.add(atributo);
+            } else if (entitieFile.get(i).trim().length() >= 7 && entitieFile.get(i).trim().substring(0, 7).equals("@Column") && cont == 0) {
+                cont++;
+                String nextLine = entitieFile.get(i + 1).trim();
+                String atributo = nextLine.substring(8, nextLine.length() - 1).replaceAll(" ", ";") + ";11;";
+                atributos.add(atributo);
+            } 
+        }
+        
+        for(String line: cgEntitie) {
+            System.out.println(line);
+        }
+        
+        manipulaArquivo.salvarArquivo(path, cgEntitie);
+        
+        GeradorDeGUI geradorDeGUI = new GeradorDeGUI(className, atributos);
+        GeradorDeDAO geradorDeDAO = new GeradorDeDAO(className, atributos);
+        
+        TrabalhadorGUI trabalhadorGUI = new TrabalhadorGUI();
         
     }
     
---
+++ LP-3_2022/src/Save/DAOTrabalhador.java
@@ -0,0 +1,63 @@
+package Save;
+
+import DAOs.DAOGenerico;
+import Entidades.Trabalhador;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DAOTrabalhador extends DAOGenerico<Trabalhador> {
+
+    private List<Trabalhador> lista = new ArrayList<>();
+
+    public DAOTrabalhador() {
+        super(Trabalhador.class);
+    }
+
+    public int autoIdTrabalhador() {
+        Integer a = (Integer) em.createQuery("SELECT MAX(e.cpf) FROM Trabalhador e ").getSingleResult();
+        if (a != null) {
+            return a + 1;
+        } else {
+            return 1;
+        }
+    }
+
+    public List<Trabalhador> listByNome(String nome) {
+        return em.createQuery("SELECT e FROM Trabalhador e WHERE e.nome LIKE :nome").setParameter("nome", "%" + nome + "%").getResultList();
+    }
+
+    public List<Trabalhador> listById(String id) {
+        return em.createQuery("SELECT e FROM Trabalhador e WHERE e.cpf= :id").setParameter("id", id).getResultList();
+    }
+
+    public List<Trabalhador> listInOrderNome() {
+        return em.createQuery("SELECT e FROM Trabalhador e ORDER BY e.nome").getResultList();
+    }
+
+    public List<Trabalhador> listInOrderPK() {
+        return em.createQuery("SELECT e FROM Trabalhador e ORDER BY e.cpf").getResultList();
+    }
+
+    public List<String> listInOrderNomeStrings(String qualOrdem) {
+        List<Trabalhador> lf;
+        if (qualOrdem.equals("id")) {
+            lf = listInOrderPK();
+        } else {
+            lf = listInOrderNome();
+        }
+
+        List<String> ls = new ArrayList<>();
+        for (int i = 0; i < lf.size(); i++) {
+            ls.add(lf.get(i).getCpf() + "-" + lf.get(i).getNome());
+        }
+        return ls;
+    }
+
+    public static void main(String[] args) {
+        DAOTrabalhador daoTrabalhador = new DAOTrabalhador();
+        List<Trabalhador> listaTrabalhador = daoTrabalhador.list();
+        for (Trabalhador trabalhador : listaTrabalhador) {
+            System.out.println(trabalhador);
+        }
+    }
+}
